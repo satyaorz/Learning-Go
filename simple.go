b9cb70915d726fd2c8d47281e65ec1b67eb99345
Satya <134395351+satyaorz@users.noreply.github.com>
2025-12-30 19:22:43 +0530

    A simple go program

---
+++ simple.go
@@ -0,0 +1,7 @@
+package main // tells the compiler to compile this file
+
+import "fmt" // import fmt (formatting) from standard library, lets us use functions like println,,,
+
+func main() {
+      fmt.println("HAPPY NEW YEAR!")
+}
